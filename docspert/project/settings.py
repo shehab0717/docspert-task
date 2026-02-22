1aab61cf414c2aebed6992c6ac02ed2eff350332
shehab0717 <shehab.abdullah12@gmail.com>
2026-02-22 13:44:34 +0200

    add: pagination with custom page size

--- docspert/project/settings.py
+++ docspert/project/settings.py
@@ -122,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "core.pagination.CustomPageSizePagination",
+}
